47525bee6d58611038369d9cc10c95a839a7e7e8
raheemadamboev <vividricemaryandmax@gmail.com>
2022-02-28 12:01:33 +0500

    showing alert dialog

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -7,6 +7,7 @@ import {
   Pressable,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert
 } from "react-native";
 import Card from "../components/Card";
 import Input from "../components/Input";
@@ -30,9 +31,19 @@ const StartGameScreen = (props: Props) => {
 
   const onConfirmEnteredNumber = () => {
     const chosenNumber = parseInt(enteredNumber);
-    console.log(chosenNumber);
     if (isNaN(chosenNumber) || chosenNumber < 1 || chosenNumber > 99) {
-      console.log("inside called");
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 1 and 99",
+        [
+          {
+            text: "Okay",
+            style: "destructive",
+            onPress: onResetEnteredNumber
+          }
+        ],
+        { cancelable: false }
+      )
       return;
     }
     setConfirmed(true);
